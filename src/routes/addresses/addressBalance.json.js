4961de47c79f99ac3291b0d9ee61935151eda46e
jeffwscott <jeffscott@outlook.com>
2021-05-06 17:38:01 -0400

    updates

--- src/routes/addresses/addressBalance.json.js
+++ src/routes/addresses/addressBalance.json.js
@@ -1,7 +1,7 @@
 export async function get(req, res, next) {
     let address = req.query.address;
-    const api_res = await global.fetch(`http://127.0.0.1:1337/states/balances/${address}`)
-
+    //const api_res = await global.fetch(`http://127.0.0.1:1337/states/balances/${address}`)
+    const api_res = await global.fetch(`http://127.0.0.1:1337/states/currency/balances/${address}`)
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(await api_res.json()))
 }
